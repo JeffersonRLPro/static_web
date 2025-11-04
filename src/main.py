536fcf0bc207dacc1ff99448d7041a8e26ca18c1
JeffersonRLPro <jeffersonriveralandaverde@gmail.com>
2025-11-04 01:04:38 -0600

    the text_node_to_html_node function and created unit tests for it

--- src/main.py
+++ src/main.py
@@ -1,9 +1,29 @@
 from textnode import TextNode, TextType
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 def main():
     test = TextNode("This is a test", TextType.LINK, "https://www.boot.dev")
     print(test)
 
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        if text_node.url == "" or text_node.url is None:
+            raise ValueError("Error: The TextType LINK, must have a valid url")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        if text_node.url == "" or text_node.url is None:
+            raise ValueError("Error: The TextType IMAGE, must have a url") 
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    # nothing was found
+    raise Exception("Error: text_node.text_type did not match any of the available text_types")
 
 if __name__ == "__main__":
     main()
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from textnode import TextType, TextNode
+from main import text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -36,6 +37,69 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.LINK)
         node2 = TextNode("This is a text node", TextType.LINK, "https://dummysite.gov")
         self.assertNotEqual(node, node2)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_italic(self):
+        node = TextNode("This is a italic text node", TextType.ITALIC)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "i")
+        self.assertEqual(html_node.value, "This is a italic text node")
+    
+    def test_code(self):
+        node = TextNode("This is a code text node", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code text node")
+    
+    def test_link(self):
+        node = TextNode("This is a link text node", TextType.LINK, "www.url.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link text node")
+        self.assertEqual(html_node.props, {"href": "www.url.com"})
+    
+    def test_image(self):
+        node = TextNode("This is an image text node", TextType.IMAGE, "www.image.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props, {"src": "www.image.com", "alt": "This is an image text node"})
+
+    def test_non_matching_textType_Enum(self):
+        node = TextNode("This should give an error", None)
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+    
+    def test_image_with_no_url(self):
+        node = TextNode("This is an image text node", TextType.IMAGE)
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    def test_link_with_no_url(self):
+        node = TextNode("This is a link text node", TextType.LINK)
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+    
+    def test_image_with_empty_string(self):
+        node = TextNode("This is an image text node", TextType.IMAGE, "")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
+
+    def test_link_with_empty_string(self):
+        node = TextNode("This is a link text node", TextType.LINK, "")
+        with self.assertRaises(ValueError):
+            text_node_to_html_node(node)
 
 if __name__ == "__main__":
     unittest.main()
