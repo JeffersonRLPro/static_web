24e013772d5cdd88c4579cc869d843c9a412ced4
JeffersonRLPro <jeffersonriveralandaverde@gmail.com>
2026-02-10 19:12:58 -0600

    fixed issues with the copy_directory.py file

--- src/copy_directory.py
+++ src/copy_directory.py
@@ -10,7 +10,6 @@ def copy_directory(source, destination):
     :param destination: Where you want the source file to be copied to
     """
     logging.basicConfig(level = logging.DEBUG)
-    # delete first
     if not os.path.exists(destination):
         # create new directory at destination
         logging.debug(f"creating directory {destination} at: {os.path.abspath(destination)}")
@@ -23,5 +22,6 @@ def copy_directory(source, destination):
             logging.debug(f"copying the file {entry} to the directory {os.path.abspath(destination)}")
             shutil.copy(entry_path, destination)
         else:
+            os.mkdir(os.path.join(destination, entry))
             logging.debug(f"going into {entry} directory")
-            copy_directory(entry_path, destination)
+            copy_directory(entry_path, os.path.join(destination, entry))
