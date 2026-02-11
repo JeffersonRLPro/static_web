70f2cab86622d9d074e873db5075508bb0ff3aec
JeffersonRLPro <jeffersonriveralandaverde@gmail.com>
2026-02-10 20:34:31 -0600

    added the new generate_site method

--- src/generate_site.py
+++ src/generate_site.py
@@ -1,7 +1,39 @@
+import os
+from markdown_to_html import markdown_to_html_node
+
 def extract_title(markdown):
+    """
+    extract_title is a method that extracts the title from a markdown file
+    
+    :param markdown: Markdown file
+    """
     split_md = markdown.split("\n")
     for line in split_md:
         if line.startswith("# "):
             title = line[2:].strip()
             return title
     raise Exception(f"No title found in provided markdown: {markdown}")
+
+def generate_page(from_path, template_path, dest_path):
+    """
+    generate_page is a method that generates a website
+    
+    :param from_path: Where the page you want to generate is located
+    :param template_path: Where the HTML template is located
+    :param dest_path: Where you want the generated html file to be located
+    """
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as file:
+        markdown = file.read()
+    with open(template_path) as f:
+        html_temp = f.read()
+    # create the html
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    title = extract_title(markdown)
+    updated_html = html_temp.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    destination_path = os.path.dirname(dest_path)
+    os.makedirs(destination_path, exist_ok = True)
+    with open(dest_path, 'w') as f1:
+        f1.write(updated_html)
+    
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 import shutil
 import os
 from copy_directory import copy_directory
+from generate_site import generate_page
 
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_directory("static", "public")
-
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 if __name__ == "__main__":
     main()
