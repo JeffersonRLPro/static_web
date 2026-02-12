5a6ef2c6d7e4e9344dd7780a2b973c40a58632cc
JeffersonRLPro <jeffersonriveralandaverde@gmail.com>
2026-02-12 01:13:48 -0600

    added the generate pages recursive method to generate multiple webpages

--- src/generate_site.py
+++ src/generate_site.py
@@ -37,3 +37,25 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, 'w') as f1:
         f1.write(updated_html)
     
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    """
+    generate_pages_recursive is a method that generates multiple pages recursively
+    
+    :param dir_path_content: the directory name
+    :param template_path: path of the template
+    :param dest_dir_path: path of the destination
+    """
+    
+    # list out everything
+    if not os.path.isfile(dir_path_content):
+        contents = os.listdir(dir_path_content)
+        for item in contents:
+            item_path = os.path.join(dir_path_content, item)
+            print(item_path)
+            if not os.path.isfile(item_path):
+                generate_pages_recursive(item_path, template_path, dest_dir_path)
+            else:
+                if item.endswith(".md"):
+                    # build dest path
+                    html_dest_path = os.path.join(dest_dir_path, f"{item_path[:-3]}.html")
+                    generate_page(item_path, template_path, html_dest_path)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import shutil
 import os
 from copy_directory import copy_directory
-from generate_site import generate_page
+from generate_site import generate_pages_recursive
 
 def main():
     if os.path.exists("public"):
         shutil.rmtree("public")
     copy_directory("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 if __name__ == "__main__":
     main()
